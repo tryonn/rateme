9353855ecfaf4100bce23c022730e085e524369f
Simão Menezes <simaomenezesneto@gmail.com>
2017-02-23 13:24:11 -0300

    Adicionando estrutura do projeto, e gitignore

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var engine = require('ejs-mate');
+var session = require('express-session');
+
+
+var app = express();
+
+app.use(express.static('public'));
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res, next){
+  res.render('index');
+});
+
+app.listen(3000, function(){
+  console.log('App running on port 3000');
+});
