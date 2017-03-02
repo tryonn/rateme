62f310ef0617133237583578106f05da014b52d2
Simão Menezes <simaomenezesneto@gmail.com>
2017-03-02 12:43:52 -0300

    Configurando ejs mate

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var engine = require('ejs-mate');
 var session = require('express-session');
+var mongoose = require('mongoose');
+var MongoStore = require('connect-mongo')(session);
 
 
 var app = express();
+
+mongoose.connect('mongodb://localhost/rateme');
 
 app.use(express.static('public'));
 app.engine('ejs', engine);
@@ -14,6 +18,12 @@ app.set('view engine', 'ejs');
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(session({
+  secret:'Thisismytestkey',
+  resave: false,
+  saveUninitialized: false
+  //store: new MongoSore({mongooseConnection: mongoose.connection})
+}));
 
 app.get('/', function(req, res, next){
   res.render('index');
